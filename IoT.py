4195cafb25cdcc31bea8a85d5ed4351fa5ad12e8
yoshitomi takashi <smartscape.iot.mail@gmail.com>
2019-08-21 18:24:33 +0900

    Update isaax.json

--- IoT.py
+++ IoT.py
@@ -1,4 +1,5 @@
 # -*- Coding: utf-8 -*-
+
 import os
 import subprocess
 import datetime
@@ -6,9 +7,9 @@ from time import sleep
  
 def main():
   dt_now = datetime.datetime.now()
-  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "Hello World from IoT device")
+  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + " Hello World from IoT device")
   print(os.environ.get('LANG'))
-  #print(os.environ.get('RemoteENV'))
+  print(os.environ.get('RemoteENV'))
  
 if __name__ == '__main__':
   while True:
