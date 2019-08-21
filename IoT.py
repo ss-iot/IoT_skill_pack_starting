17d5604788df5d519f09682e94eb110cd9b4c2ff
yoshitomi takashi <smartscape.iot.mail@gmail.com>
2019-08-21 18:44:15 +0900

    Update

--- IoT.py
+++ IoT.py
@@ -1,16 +1,15 @@
 # -*- Coding: utf-8 -*-
-
 import os
 import subprocess
 import datetime
 from time import sleep
- 
+
 def main():
   dt_now = datetime.datetime.now()
-  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + " Hello World from IoT device")
+  print(dt_now.strftime('%Y/%m/%d %H:%M:%S') + "IoT Device Online")
   print(os.environ.get('LANG'))
   #print(os.environ.get('RemoteENV'))
- 
+
 if __name__ == '__main__':
   while True:
     main()
